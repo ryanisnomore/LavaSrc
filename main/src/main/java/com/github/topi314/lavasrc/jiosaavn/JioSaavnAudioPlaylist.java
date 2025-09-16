934f12d706491919b9c83dc48d38e37445d48d9b
RY4N <ryan.is.nomore7@gmail.com>
2025-09-16 14:35:17 +0600

    Update JioSaavnAudioPlaylist.java

--- main/src/main/java/com/github/topi314/lavasrc/jiosaavn/JioSaavnAudioPlaylist.java
+++ main/src/main/java/com/github/topi314/lavasrc/jiosaavn/JioSaavnAudioPlaylist.java
@@ -1,13 +1,39 @@
 package com.github.topi314.lavasrc.jiosaavn;
 
-import com.github.topi314.lavasrc.ExtendedAudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+
 import java.util.List;
 
-public class JioSaavnAudioPlaylist extends ExtendedAudioPlaylist {
-	public JioSaavnAudioPlaylist(
-		String name, List<AudioTrack> tracks, ExtendedAudioPlaylist.Type type, String identifier, String artworkURL, String author, Integer totalTracks
-	) {
-		super(name, tracks, type, identifier, artworkURL, author, totalTracks);
-	}
+public class JioSaavnAudioPlaylist implements AudioPlaylist {
+    
+    private final String name;
+    private final List<AudioTrack> tracks;
+    private final boolean isAlbum;
+    
+    public JioSaavnAudioPlaylist(String name, List<AudioTrack> tracks, boolean isAlbum) {
+        this.name = name;
+        this.tracks = tracks;
+        this.isAlbum = isAlbum;
+    }
+    
+    @Override
+    public String getName() {
+        return this.name;
+    }
+    
+    @Override
+    public List<AudioTrack> getTracks() {
+        return this.tracks;
+    }
+    
+    @Override
+    public AudioTrack getSelectedTrack() {
+        return null;
+    }
+    
+    @Override
+    public boolean isSearchResult() {
+        return false;
+    }
 }
