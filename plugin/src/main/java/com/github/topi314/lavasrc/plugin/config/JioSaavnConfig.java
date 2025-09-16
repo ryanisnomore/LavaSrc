8aa76d743df52e322e2a3b525141ce5d8f80780a
RY4N <ryan.is.nomore7@gmail.com>
2025-09-16 14:36:39 +0600

    Update JioSaavnConfig.java

--- plugin/src/main/java/com/github/topi314/lavasrc/plugin/config/JioSaavnConfig.java
+++ plugin/src/main/java/com/github/topi314/lavasrc/plugin/config/JioSaavnConfig.java
@@ -1,50 +1,37 @@
 package com.github.topi314.lavasrc.plugin.config;
 
-import com.github.topi314.lavasrc.jiosaavn.JioSaavnAudioSourceManager;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@ConfigurationProperties(prefix = "plugins.lavasrc.jiosaavn")
+@ConfigurationProperties(prefix = "jiosaavn")
 @Component
 public class JioSaavnConfig {
-	@Nullable
-	private HttpProxyConfig proxy;
-	private JioSaavnDecryptionConfig decryption;
+    private String apiURL = "https://ryan-jiosaavn.vercel.app/api";
+    private int playlistTrackLimit = 50;
+    private int recommendationsTrackLimit = 10;
 
-	@Nullable
-	public HttpProxyConfig getProxy() {
-		return this.proxy;
-	}
+    public String getApiURL() {
+        return apiURL;
+    }
 
-	@SuppressWarnings("unused")
-	public void setProxy(@Nullable HttpProxyConfig proxy) {
-		this.proxy = proxy;
-	}
+    public void setApiURL(String apiURL) {
+        this.apiURL = apiURL;
+    }
 
-	public JioSaavnDecryptionConfig getDecryption() {
-		return decryption;
-	}
+    public int getPlaylistTrackLimit() {
+        return playlistTrackLimit;
+    }
 
-	@SuppressWarnings("unused")
-	public void setDecryption(JioSaavnDecryptionConfig decryption) {
-		this.decryption = decryption;
-	}
+    public void setPlaylistTrackLimit(int playlistTrackLimit) {
+        this.playlistTrackLimit = playlistTrackLimit;
+    }
 
-	public @NotNull JioSaavnAudioSourceManager.JioSaavnConfig buildConfig() {
-		JioSaavnDecryptionConfig decryptionConfig = getDecryption();
-		if (decryptionConfig == null || decryptionConfig.getSecretKey() == null) {
-			throw new IllegalStateException("JioSaavn is enabled, but JioSaavn secret key is not provided");
-		}
+    public int getRecommendationsTrackLimit() {
+        return recommendationsTrackLimit;
+    }
 
-		JioSaavnAudioSourceManager.JioSaavnConfig sourceConfig = new JioSaavnAudioSourceManager.JioSaavnConfig(decryptionConfig.getSecretKey());
-		if (decryptionConfig.getAlgorithm() != null) {
-			sourceConfig.setAlgorithm(decryptionConfig.getAlgorithm());
-		}
-		if (decryptionConfig.getTransformation() != null) {
-			sourceConfig.setTransformation(decryptionConfig.getTransformation());
-		}
-		return sourceConfig;
-	}
+    public void setRecommendationsTrackLimit(int recommendationsTrackLimit) {
+        this.recommendationsTrackLimit = recommendationsTrackLimit;
+    }
 }
